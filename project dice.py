01f73c28c061226eb7d76bf9af0caad893610ff1
Mortal coder <74857321+mortalcoder@users.noreply.github.com>
2021-01-25 14:12:15 +0530

    Update project dice.py

--- project dice.py
+++ project dice.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         listw = list("123456")
         e = random.choice(listw) #random is the freakin' module that helps u to print random numbers ;)
         print(b+"outcome /",e + g) 
-        a = str(input('Wanna roll me again?[y/n]')
+        a = str(input('Wanna roll me again?[enter/n]')
         if a == 'n'
             exit()
         else:
